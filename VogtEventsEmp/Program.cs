43455dd549c1ebce0fd99fe2d5e5f00c7b0517a6
gavogt <gvogt@asu.edu>
2018-09-08 15:17:28 -0700

    Add project files.

---
+++ VogtEventsEmp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogtEventsEmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayHeader();
+            DisplayGreeting();
+
+            Employee emp = new Employee();
+
+
+
+        }
+
+        class Employee
+        {
+
+            private DisplayEmployeeInformation DisplayInformation;
+
+        }
+
+        class DisplayEmployeeInformation
+        {
+
+        }
+
+        public static void ShowUserGreeting(string userName)
+        {
+            Console.WriteLine($"Greetins {userName} please select options");
+
+        }
+
+        public static void DisplayHeader() => Console.WriteLine("**************EMPLOYEE SOLUTION**************");
+        public static void DisplayGreeting() => Console.WriteLine("Welcome to the employee solution!");
+
+    }
+}
